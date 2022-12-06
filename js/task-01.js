c0d379b64092e23d8af52f08bcda4c2a1a44b728
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-06 19:42:11 +0100

    Elements length

--- js/task-01.js
+++ js/task-01.js
@@ -1,22 +1,13 @@
-const categories = document.querySelectorAll('h2');
+const categories = document.querySelectorAll('#categories h2');
 const categoriesArray = Array.from(categories).length;
-
 
 console.log(`Number of categories: ${categoriesArray}`);
 
-categories.forEach((el)=>{
+categories.forEach((el, i)=>{
     console.log(`Category: ${el.textContent}`);
+    const x = document.getElementsByClassName("item")[i];
+    const y = x.getElementsByTagName('li');
+const yArray = Array.from(y).length
+    console.log(`Elements: ${yArray}`);
 });
 
-
-
-
-
-
-// const category = document.querySelectorAll('h2');
-// const categoryArray = Array.from(category);
-// category.forEach((elem)=>console.log(`Category: ${elem}`));
-
-// console.log(category);
-// console.log(`Category: ${category}`);
-console.log(`Elements: `);
