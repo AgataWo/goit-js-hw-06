6a1e35a767ad2cd3d068348c8619507281c217e5
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-06 17:31:23 +0100

    category name

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,22 @@
 const categories = document.querySelectorAll('h2');
 const categoriesArray = Array.from(categories).length;
 
+
 console.log(`Number of categories: ${categoriesArray}`);
+
+categories.forEach((el)=>{
+    console.log(`Category: ${el.textContent}`);
+});
+
+
+
+
+
+
+// const category = document.querySelectorAll('h2');
+// const categoryArray = Array.from(category);
+// category.forEach((elem)=>console.log(`Category: ${elem}`));
+
+// console.log(category);
+// console.log(`Category: ${category}`);
+console.log(`Elements: `);
