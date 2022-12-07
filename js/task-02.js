bc5e140c05b43b38ce347854b7f48460d97989fd
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-07 17:53:33 +0100

    add array elements to <li>

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let list = document.querySelector("#ingredients");
+ingredients.forEach((item)=>{
+  let li =document.createElement("li");
+  li.innerText =item;
+  li.classList.add("item");
+  list.appendChild(li);
+});
