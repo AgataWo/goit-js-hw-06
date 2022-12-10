c806ff323d3c20d9fee152539fc860234f73eb33
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-10 21:16:31 +0100

    add action

--- js/task-04.js
+++ js/task-04.js
@@ -2,3 +2,19 @@ const buttonDecrement = document.querySelector('button[data-action="decrement"]'
 const buttonIncrement = document.querySelector('button[data-action="increment"]');
 const value = document.getElementById('value');
 
+const valueNumber = parseInt(value.textContent);
+
+let counterValue = valueNumber;
+
+const handleClickDec = ()=>{
+    counterValue-=1;
+    value.innerHTML = `<span id=value>${counterValue}</span>`
+};
+const handleClickInc = ()=>{
+    counterValue+=1;
+    value.innerHTML = `<span id=value>${counterValue}</span>`
+};
+
+
+buttonDecrement.addEventListener("click", handleClickDec);
+buttonIncrement.addEventListener("click", handleClickInc);
