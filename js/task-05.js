07f51e9a4b270f49f9a4aed57f0e36b95433fa5b
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-10 21:57:57 +0100

    add action

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,11 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
+
+
 nameInput.addEventListener('input', e=>{
-    
-    console.log(e)});
+    if(e.currentTarget.value){
+nameOutput.textContent = e.currentTarget.value;
+}else nameOutput.textContent="Anonymous";
+});
+
