5a2f2ca6a76e124bac938749ad5c3ebf61c11277
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-11 18:42:37 +0100

    task 7 done

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector('#font-size-control');
+const content = document.querySelector('#text');
+
+slider.addEventListener('input', function(){
+const size = slider.value;
+    content.style.fontSize = size + "px";
+    console.log(size);
+});
