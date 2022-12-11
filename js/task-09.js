b13750ac5998c1a342be9cb85ba8ca498ed9dff0
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-11 19:21:01 +0100

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color");
+const bgColor = document.querySelector(".color")
+
+function changeBackground(color) {
+  document.body.style.background = color;
+  bgColor.innerHTML=`<span class="color">${color}</span>`;
+}
+
+button.addEventListener("click",function() { changeBackground(getRandomHexColor()) });
